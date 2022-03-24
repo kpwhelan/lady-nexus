2ee10e0d0c12b26a3eba67e77c733a32e9b3675c
Kevin Whelan <kevinw@mobilefuse.com>
2022-03-23 20:11:48 -0400

    add logo images, register view ready, updated color in components

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
 
     theme: {
         extend: {
+            colors: {
+                sage: '#5f7d54',
+            },
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },

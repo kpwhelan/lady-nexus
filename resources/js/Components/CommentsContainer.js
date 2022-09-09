1b82f8c2ff48869972b054d8738097db5ac584a2
Kevin Whelan <kevinwhelandev@gmail.com>
2022-09-09 14:01:29 -0400

    create skeleton comment component

---
+++ resources/js/Components/CommentsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CommentsContainer() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CommentsContainer
--- resources/js/Components/Post.js
+++ resources/js/Components/Post.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function Post({ post }) {
-  return (
+    const [showComments, setShowComments] = useState(false);
+
+    return (
     <div className="w-2/3 rounded overflow-scroll shadow-lg m-5 transition ease-in-out delay-110 hover:-translate-y-2 hover:scale-102">
         <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">
@@ -13,10 +15,12 @@ function Post({ post }) {
         </div>
         <div className="px-6 pt-4 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 cursor-default">#{post.category.name}</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 cursor-pointer transition ease-in-out delay-110 hover:-translate-y-1 hover:scale-110 hover:bg-sage hover:text-white duration-300">{post.comments.length} comments</span>
+            <span onClick={setShowComments(true)} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 cursor-pointer transition ease-in-out delay-110 hover:-translate-y-1 hover:scale-110 hover:bg-sage hover:text-white duration-300">{post.comments.length} comments</span>
         </div>
+
+
     </div>
-  )
+    )
 }
 
 export default Post

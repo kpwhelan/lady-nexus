450d53b829cb4e3fc0cb97c994847cfaa1674feb
Kevin Whelan <kevinwhelandev@gmail.com>
2022-10-11 19:33:21 -0400

    more styling updates for invite email

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -14,8 +14,8 @@
     </head>
     <body class="antialiased bg-slate-200">
         <img src={{asset('images/LadyNexus-logo.png')}} class="block h-52 w-52 rounded-full mx-auto mt-4" style="display: block; height:13rem; width:13rem; border-radius: 9999px; margin-left:auto; margin-right:auto; margin-top:1rem" />
-        <div class="mx-auto bg-white p-3 mt-4 w-fit">
-            <p class="text-lg">Hi,</p>
+        <div class="mx-auto bg-white p-3 mt-4 w-fit" style="margin-left:auto; margin-right:auto; background-color: rgb(255 255 255 / var(--tw-bg-opacity)); --tw-bg-opacity: 1; padding: 0.75rem; margin-top: 1rem;  width: fit-content;">
+            <p class="text-lg" style="font-size: 1.125rem; line-height: 1.75rem">Hi,</p>
             <p>{{$name}} has invited you to join Lady Nexus, a safe, online platform  just for women.</p>
             <p>Please copy the token code below, you'll need when you register at <a class="underline text-lg" href="{{route('register')}}">Lady Nexus</a>.</p>
             <br>

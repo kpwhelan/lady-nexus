441cd3fb6be612810dfef0a4b1c45343bbd84c5e
Kevin Whelan <kevinwhelandev@gmail.com>
2022-10-11 19:29:52 -0400

    update styling of invite email for certain browsers

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -13,7 +13,7 @@
 
     </head>
     <body class="antialiased bg-slate-200">
-        <img src={{asset('images/LadyNexus-logo.png')}} class="block h-52 w-52 rounded-full mx-auto mt-4" />
+        <img src={{asset('images/LadyNexus-logo.png')}} class="block h-52 w-52 rounded-full mx-auto mt-4" style="display: block; height:13rem; width:13rem; border-radius: 9999px; margin-left:auto; margin-right:auto; margin-top:1rem" />
         <div class="mx-auto bg-white p-3 mt-4 w-fit">
             <p class="text-lg">Hi,</p>
             <p>{{$name}} has invited you to join Lady Nexus, a safe, online platform  just for women.</p>

65f7ae2c2b9f9c9f223aeeaef2fd0bacb014b050
Kevin Whelan <kevinwhelandev@gmail.com>
2022-10-11 16:23:00 -0400

    handle null urls

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,7 +35,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         $user = $request->user();
-        if ($user && $user->profile_picture_url) {
+        if ($user && $user->profile_picture_url != null) {
             $user->profile_picture_url = Storage::temporaryUrl($user->profile_picture_url, now()->addHours(24));
         }
 
